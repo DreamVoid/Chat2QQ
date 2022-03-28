c372c0b122dd1c388cc017f6d900fc50a65448d8
haoen <7098430+haoenz@users.noreply.github.com>
2022-03-28 22:33:02 +0800

    修正前缀使用方式

--- Chat2QQ-Bukkit/src/main/java/me/dreamvoid/chat2qq/bukkit/listener/onGroupMessage.java
+++ Chat2QQ-Bukkit/src/main/java/me/dreamvoid/chat2qq/bukkit/listener/onGroupMessage.java
@@ -48,7 +48,7 @@ public class onGroupMessage implements Listener {
             for(String prefix : plugin.getConfig().getStringList("bot.requite-special-word-prefix.prefix")){
                 if(e.getMessage().startsWith(prefix)){
                     allowPrefix = true;
-                    formatText = formatText.replace(prefix,"");
+                    formatText = formatText.substring(1);
                     break;
                 }
             }
