261b39e5d82c2c27d30783a75e5c3048a8cf417a
流光溢彩Last <dreamvoid@dreamvoid.ml>
2022-07-07 19:31:35 +0800

    修复从群内发送消息时插件处理了错误的消息的问题

--- Chat2QQ-Bukkit/src/main/java/me/dreamvoid/chat2qq/bukkit/listener/onGroupMessage.java
+++ Chat2QQ-Bukkit/src/main/java/me/dreamvoid/chat2qq/bukkit/listener/onGroupMessage.java
@@ -2,15 +2,12 @@ package me.dreamvoid.chat2qq.bukkit.listener;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.dreamvoid.chat2qq.bukkit.BukkitPlugin;
-import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.api.MiraiMC;
-import me.dreamvoid.miraimc.bukkit.event.MiraiGroupMessageEvent;
+import me.dreamvoid.miraimc.bukkit.event.message.passive.MiraiGroupMessageEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
-import java.util.UUID;
 
 public class onGroupMessage implements Listener {
     private final BukkitPlugin plugin;
@@ -21,38 +18,40 @@ public class onGroupMessage implements Listener {
     @EventHandler
     public void onGroupMessageReceive(MiraiGroupMessageEvent e){
         String name = e.getSenderNameCard();
-        if(name.equalsIgnoreCase("") && plugin.getConfig().getBoolean("general.use-nick-if-namecard-null",false) && e.getType() == 0){
-            name = MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).getMember(e.getSenderID()).getNick();
+        if(name.equalsIgnoreCase("") && plugin.getConfig().getBoolean("general.use-nick-if-namecard-null",false)){
+            name = e.getSenderName();
         }
-        String formatText;
-        if(plugin.getConfig().getBoolean("general.use-miraimc-bind",true) && !MiraiMC.getBinding(e.getSenderID()).equals("")){
-            formatText = plugin.getConfig().getString("general.bind-chat-format")
-                    .replace("%groupname%",e.getGroupName())
-                    .replace("%groupid%",String.valueOf(e.getGroupID()))
-                    .replace("%nick%",name)
-                    .replace("%qq%",String.valueOf(e.getSenderID()))
-                    .replace("%message%",e.getMessage());
-            if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null){
-                formatText = PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(UUID.fromString(MiraiMC.getBinding(e.getSenderID()))),formatText);
-            }
-        } else formatText = plugin.getConfig().getString("general.in-game-chat-format")
-                    .replace("%groupname%",e.getGroupName())
-                    .replace("%groupid%",String.valueOf(e.getGroupID()))
-                    .replace("%nick%",name)
-                    .replace("%qq%",String.valueOf(e.getSenderID()))
-                    .replace("%message%",e.getMessage());
 
+        String message = e.getMessage();
         // 判断消息是否带前缀
         boolean allowPrefix = false;
         if(plugin.getConfig().getBoolean("bot.requite-special-word-prefix.enabled",false)){
             for(String prefix : plugin.getConfig().getStringList("bot.requite-special-word-prefix.prefix")){
                 if(e.getMessage().startsWith(prefix)){
                     allowPrefix = true;
-                    formatText = formatText.substring(prefix.length());
+                    message = message.substring(prefix.length());
                     break;
                 }
             }
         } else allowPrefix = true;
+
+        String formatText;
+        if(plugin.getConfig().getBoolean("general.use-miraimc-bind",true) && MiraiMC.getBind(e.getSenderID()) != null){
+            formatText = plugin.getConfig().getString("general.bind-chat-format")
+                    .replace("%groupname%",e.getGroupName())
+                    .replace("%groupid%",String.valueOf(e.getGroupID()))
+                    .replace("%nick%",name)
+                    .replace("%qq%",String.valueOf(e.getSenderID()))
+                    .replace("%message%", message);
+            if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null){
+                formatText = PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(MiraiMC.getBind(e.getSenderID())), formatText);
+            }
+        } else formatText = plugin.getConfig().getString("general.in-game-chat-format")
+                    .replace("%groupname%",e.getGroupName())
+                    .replace("%groupid%",String.valueOf(e.getGroupID()))
+                    .replace("%nick%",name)
+                    .replace("%qq%",String.valueOf(e.getSenderID()))
+                    .replace("%message%", message);
 
         if(plugin.getConfig().getLongList("bot.bot-accounts").contains(e.getBotID()) && plugin.getConfig().getLongList("bot.group-ids").contains(e.getGroupID()) && allowPrefix){
             Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',formatText));
--- Chat2QQ-Bungee/src/main/java/me/dreamvoid/chat2qq/bungee/listener/onGroupMessage.java
+++ Chat2QQ-Bungee/src/main/java/me/dreamvoid/chat2qq/bungee/listener/onGroupMessage.java
@@ -1,15 +1,12 @@
 package me.dreamvoid.chat2qq.bungee.listener;
 
 import me.dreamvoid.chat2qq.bungee.BungeePlugin;
-import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.api.MiraiMC;
-import me.dreamvoid.miraimc.bungee.event.MiraiGroupMessageEvent;
+import me.dreamvoid.miraimc.bungee.event.message.passive.MiraiGroupMessageEvent;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
-
-import java.util.UUID;
 
 public class onGroupMessage implements Listener {
     private final BungeePlugin plugin;
@@ -20,40 +17,43 @@ public class onGroupMessage implements Listener {
     @EventHandler
     public void onGroupMessageReceive(MiraiGroupMessageEvent e){
         String name = e.getSenderNameCard();
-        if(name.equalsIgnoreCase("") && plugin.getConfig().getBoolean("general.use-nick-if-namecard-null",false) && e.getType() == 0){
-            name = MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).getMember(e.getSenderID()).getNick();
+        if(name.equalsIgnoreCase("") && plugin.getConfig().getBoolean("general.use-nick-if-namecard-null",false)){
+            name = e.getSenderName();
         }
-        String formatText;
-        if(plugin.getConfig().getBoolean("general.use-miraimc-bind",true) && !MiraiMC.getBinding(e.getSenderID()).equals("")){
-            formatText = plugin.getConfig().getString("general.bind-chat-format")
-                    .replace("%groupname%",e.getGroupName())
-                    .replace("%groupid%",String.valueOf(e.getGroupID()))
-                    .replace("%nick%",name)
-                    .replace("%qq%",String.valueOf(e.getSenderID()))
-                    .replace("%message%",e.getMessage())
-                    .replace("%player%",plugin.getProxy().getPlayer(UUID.fromString(MiraiMC.getBinding(e.getSenderID()))).getDisplayName());
-        } else formatText = plugin.getConfig().getString("general.in-game-chat-format")
-                    .replace("%groupname%",e.getGroupName())
-                    .replace("%groupid%",String.valueOf(e.getGroupID()))
-                    .replace("%nick%",name)
-                    .replace("%qq%",String.valueOf(e.getSenderID()))
-                    .replace("%message%",e.getMessage());
 
+        String message = e.getMessage();
         // 判断消息是否带前缀
         boolean allowPrefix = false;
         if(plugin.getConfig().getBoolean("bot.requite-special-word-prefix.enabled",false)){
             for(String prefix : plugin.getConfig().getStringList("bot.requite-special-word-prefix.prefix")){
                 if(e.getMessage().startsWith(prefix)){
                     allowPrefix = true;
-                    formatText = formatText.substring(prefix.length());
+                    message = message.substring(prefix.length());
                     break;
                 }
             }
         } else allowPrefix = true;
 
+        String formatText;
+        if(plugin.getConfig().getBoolean("general.use-miraimc-bind",true) && MiraiMC.getBind(e.getSenderID()) != null){
+            formatText = plugin.getConfig().getString("general.bind-chat-format")
+                    .replace("%groupname%",e.getGroupName())
+                    .replace("%groupid%",String.valueOf(e.getGroupID()))
+                    .replace("%nick%",name)
+                    .replace("%qq%",String.valueOf(e.getSenderID()))
+                    .replace("%message%", message)
+                    .replace("%player%",plugin.getProxy().getPlayer(MiraiMC.getBind(e.getSenderID())).getDisplayName());
+        } else {
+            formatText = plugin.getConfig().getString("general.in-game-chat-format")
+                    .replace("%groupname%",e.getGroupName())
+                    .replace("%groupid%",String.valueOf(e.getGroupID()))
+                    .replace("%nick%",name)
+                    .replace("%qq%",String.valueOf(e.getSenderID()))
+                    .replace("%message%", message);
+        }
+
         if(plugin.getConfig().getLongList("bot.bot-accounts").contains(e.getBotID()) && plugin.getConfig().getLongList("bot.group-ids").contains(e.getGroupID()) && allowPrefix){
             plugin.getProxy().broadcast(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&',formatText)));
         }
     }
-
 }
