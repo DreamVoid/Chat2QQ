a3c0ca8c0f9a3e42b5f2cebb72e920998832eb0a
DreamVoid <45266046+DreamVoid@users.noreply.github.com>
2022-07-07 19:31:35 +0800

    修复从群内发送消息时插件处理了错误的消息的问题

--- Chat2QQ-Nukkit/src/main/java/me/dreamvoid/chat2qq/nukkit/listener/onGroupMessage.java
+++ Chat2QQ-Nukkit/src/main/java/me/dreamvoid/chat2qq/nukkit/listener/onGroupMessage.java
@@ -6,7 +6,7 @@ import cn.nukkit.utils.TextFormat;
 import me.dreamvoid.chat2qq.nukkit.NukkitPlugin;
 import me.dreamvoid.miraimc.api.MiraiBot;
 import me.dreamvoid.miraimc.api.MiraiMC;
-import me.dreamvoid.miraimc.nukkit.event.MiraiGroupMessageEvent;
+import me.dreamvoid.miraimc.nukkit.event.message.passive.MiraiGroupMessageEvent;
 
 public class onGroupMessage implements Listener {
     private final NukkitPlugin plugin;
@@ -16,36 +16,38 @@ public class onGroupMessage implements Listener {
 
     @EventHandler
     public void onGroupMessageReceive(MiraiGroupMessageEvent e){
-        boolean allowPrefix = false;
         String name = e.getSenderNameCard();
         if(name.equalsIgnoreCase("") && plugin.getConfig().getBoolean("general.use-nick-if-namecard-null",false)){
             name = MiraiBot.getBot(e.getBotID()).getGroup(e.getGroupID()).getMember(e.getSenderID()).getNick();
         }
+
+        String message = e.getMessage();
+        // 判断消息是否带前缀
+        boolean allowPrefix = false;
+        if(plugin.getConfig().getBoolean("bot.requite-special-word-prefix.enabled",false)){
+            for(String prefix : plugin.getConfig().getStringList("bot.requite-special-word-prefix.prefix")){
+                if(e.getMessage().startsWith(prefix)){
+                    allowPrefix = true;
+                    message = message.substring(prefix.length());
+                    break;
+                }
+            }
+        } else allowPrefix = true;
+
         String formatText;
-        if(plugin.getConfig().getBoolean("general.use-miraimc-bind",true) && !MiraiMC.getBinding(e.getSenderID()).equals("")){
+        if(plugin.getConfig().getBoolean("general.use-miraimc-bind",true) && MiraiMC.getBind(e.getSenderID()) != null){
             formatText = plugin.getConfig().getString("general.bind-chat-format")
                     .replace("%groupname%",e.getGroupName())
                     .replace("%groupid%",String.valueOf(e.getGroupID()))
                     .replace("%nick%",name)
                     .replace("%qq%",String.valueOf(e.getSenderID()))
-                    .replace("%message%",e.getMessage());
+                    .replace("%message%", message);
         } else formatText = plugin.getConfig().getString("general.in-game-chat-format")
                     .replace("%groupname%",e.getGroupName())
                     .replace("%groupid%",String.valueOf(e.getGroupID()))
                     .replace("%nick%",name)
                     .replace("%qq%",String.valueOf(e.getSenderID()))
-                    .replace("%message%",e.getMessage());
-
-        // 判断消息是否带前缀
-        if(plugin.getConfig().getBoolean("bot.requite-special-word-prefix.enabled",false)){
-            for(String prefix : plugin.getConfig().getStringList("bot.requite-special-word-prefix.prefix")){
-                if(e.getMessage().startsWith(prefix)){
-                    allowPrefix = true;
-                    formatText = formatText.substring(prefix.length());
-                    break;
-                }
-            }
-        } else allowPrefix = true;
+                    .replace("%message%", message);
 
         if(plugin.getConfig().getLongList("bot.bot-accounts").contains(e.getBotID()) && plugin.getConfig().getLongList("bot.group-ids").contains(e.getGroupID()) && allowPrefix){
             plugin.getServer().broadcastMessage(TextFormat.colorize('&',formatText));
