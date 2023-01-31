8fc4f7cb24d4fce82445aef5b4dceddca602cd3f
ApliNi <59365724+ApliNi@users.noreply.github.com>
2023-01-31 13:54:52 +0800

    添加消息处理功能 (#67)
    * 添加消息预处理功能
    
    * 删除无用代码
    
    * 添加匹配方式: 完全匹配
    
    * 修复配置路径错误
    
    * 补充一个功能的示例配置
    
    * 添加运行指令功能
    
    * 添加运行指令功能
    
    * 删除未实现的功能的注释
    
    * 添加功能和错误修复
    
    * 删除执行指令相关的功能
    
    * 删除执行指令相关的功能
    
    * 清理多余的 import
    
    * 删除多余的 InterruptedException

--- Chat2QQ-Bukkit/src/main/java/me/dreamvoid/chat2qq/bukkit/listener/onGroupMessage.java
+++ Chat2QQ-Bukkit/src/main/java/me/dreamvoid/chat2qq/bukkit/listener/onGroupMessage.java
@@ -9,7 +9,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import java.util.Objects;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,7 +20,7 @@ public class onGroupMessage implements Listener {
     }
 
     @EventHandler
-    public void onGroupMessageReceive(MiraiGroupMessageEvent e){
+    public void onGroupMessageReceive(MiraiGroupMessageEvent e) {
         if(plugin.getConfig().getStringList("blacklist.word").stream().anyMatch(s -> e.getMessage().contains(s)) || plugin.getConfig().getLongList("blacklist.qq").contains(e.getSenderID())) return;
 
         String name = e.getSenderNameCard();
@@ -55,6 +55,60 @@ public class onGroupMessage implements Listener {
 //                        .replace("%regex_nick%", "%regex_nick%")
                         .replace("%qq%",String.valueOf(e.getSenderID()))
                         .replace("%message%", message);
+            }
+        }
+
+        // 预处理
+        if(plugin.getConfig().getBoolean("general.pretreatment.enabled",false)){
+            for(Map<?, ?> config : plugin.getConfig().getMapList("general.pretreatment.list")){
+                // 前缀匹配
+                if(config.get("prefix") != null && message.startsWith((String) config.get("prefix"))){
+                    if(config.get("send") != null){
+                        return;
+                    }
+                    else if(config.get("to_all") != null){
+                        message = (String) config.get("to_all");
+                    }
+                    else if(config.get("to_replace") != null){
+                        message = message.replace((String) config.get("prefix"), (String) config.get("to_replace"));
+                    }
+                }
+
+                // 包含
+                else if(config.get("contain") != null && message.contains((String) config.get("contain"))){
+                    if(config.get("send") != null){
+                        return;
+                    }
+                    else if(config.get("to_replace") != null){
+                        message = message.replace((String) config.get("contain"), (String) config.get("to_replace"));
+                    }
+                    else if(config.get("to_all") != null){
+                        message = (String) config.get("to_all");
+                    }
+                }
+
+                // 相等
+                else if(config.get("equal") != null && Objects.equals(message, config.get("equal"))){
+                    if(config.get("send") != null){
+                        return;
+                    }
+                    else if(config.get("to_all") != null){
+                        message = (String) config.get("to_all");
+                    }
+                }
+
+                // 正则匹配
+                else if(config.get("regular") != null && Pattern.compile((String) config.get("regular")).matcher(message).find()){
+                    if(config.get("send") != null){
+                        return;
+                    }
+                    else if(config.get("to_regular") != null){
+                        message = message.replaceAll((String) config.get("regular"), (String) config.get("to_regular"));
+                    }
+                    else if(config.get("to_all") != null){
+                        message = (String) config.get("to_all");
+                    }
+                }
             }
         }
 
